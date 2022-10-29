a2b508a9257d64558372e30f8c33421ffceca89f
ericdc58 <heimricusdc@gmail.com>
2022-10-29 09:31:53 +0200

    Enhanced the fetch of the main and some css styles

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 const filmsContainer = document.getElementById(`allFilms`);
 
+const api_Url = "https://swapi.dev/api/films/";
+const photo_Url = "https://starwars-visualguide.com/assets/img/films/";
+let photo_id = 1;
+
 
 /*----FUNCIÓ PER COMUNICAR AMB API I VEURE PELICULES----*/
 const mostrar_Pelicules = () => {
-    fetch("https://swapi.dev/api/films/").then(response => response.json()).then( data => {
-            console.log(data.results[0].title);
+    fetch(api_Url).then(response => response.json()).then( data => {
+            data.results.forEach(pelicula => {
+                filmsContainer.innerHTML += `
+                <article>
+                    <h4>
+                        ${pelicula.title}
+                    </h4>
+                    <figure>
+                        <img src="${photo_Url}${photo_id}.jpg" alt="${pelicula.title}'s poster " />
+                    </figure>
+                    <p>
+                        ${pelicula.opening_crawl}
+                    </p>
+                    <h5>In this movie appears:</h5>
+                    <p>
+                        ${pelicula.characters.length} characters
+                    </p>
+                    <p>
+                        ${pelicula.planets.length} planets
+                    </p>
+                    <p>
+                        ${pelicula.starships.length} spaceships
+                    </p>
+                    <p>
+                        ${pelicula.vehicles.length} vehicles
+                    </p>
+                    <p>
+                        ${pelicula.species.length} species
+                    </p>
+                </article>`
+                photo_id++;
+            });
         }
     );
     
